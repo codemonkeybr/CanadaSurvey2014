4b1162727a11e850c017cef64bf5fd36ffffe7ff
Tesso Martins Costa a.k.a CodeMonkeyBR <tesso.martins@gmail.com>
2014-05-14 23:35:46 -0300

    Start of the implementation of the gmaps, too sleepy to continue... must ..sleep now..

--- src/main/java/br/com/tesso/canadasurvey/controller/SurveyController.java
+++ src/main/java/br/com/tesso/canadasurvey/controller/SurveyController.java
@@ -5,12 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
 
 import br.com.tesso.canadasurvey.entity.SurveyInfo;
 
@@ -20,6 +26,15 @@ import br.com.tesso.canadasurvey.entity.SurveyInfo;
 public class SurveyController {
 	
 	private List<SurveyInfo> listSurveyInfos;
+	
+	
+	private MapModel advancedModel;
+	
+	public SurveyController(){
+		
+		
+		//advancedModel.addOverlay(new Marker);
+	}
 	
 	@PostConstruct
 	public void carregar(){
@@ -45,15 +60,25 @@ public class SurveyController {
 			        // use comma as separator
 				String[] data = line.split(cvsSplitBy);
 	 
-				System.out.println("City [code= " + data[1] 
-	                                 + " , name=" + data[5] + "]");
+				System.out.println("City [name= " + data[1] 
+	                                 + " , Population=" + data[2] + "]");
 				
 				survey.setRank(new Integer(data[0]));
 				survey.setName(data[1]);
 				survey.setPopulation(new Float(data[2]));
-				
-	 
+				survey.setPopulationGrowth(new Float(data[3].replace("%", "")));
+				survey.setUnemploymentRate(new Float(data[4]));
+				survey.setAverageHouseholdIncome(new BigDecimal(data[5].replace(",", "").replace("$", "")));
+				if(data.length > 30) {
+					survey.setLatitude(new Float(data[30]));
+					survey.setLongitude(new Float(data[31]));
+				}
+				listSurveyInfos.add(survey);
 			}
+			
+			advancedModel = new DefaultMapModel();
+			LatLng coord1 = new LatLng(listSurveyInfos.get(0).getLatitude(), listSurveyInfos.get(0).getLongitude());
+			advancedModel.addOverlay(new Marker(coord1, listSurveyInfos.get(0).getName()));
 	 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -71,5 +96,23 @@ public class SurveyController {
 	 
 		System.out.println("Done");
 	}
+
+	public List<SurveyInfo> getListSurveyInfos() {
+		return listSurveyInfos;
+	}
+
+	public void setListSurveyInfos(List<SurveyInfo> listSurveyInfos) {
+		this.listSurveyInfos = listSurveyInfos;
+	}
+
+	public MapModel getAdvancedModel() {
+		return advancedModel;
+	}
+
+	public void setAdvancedModel(MapModel advancedModel) {
+		this.advancedModel = advancedModel;
+	}
+	
+	
 
 }
